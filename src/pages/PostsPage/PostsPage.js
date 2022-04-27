c59c480262cf630431d56d6e0d879d7c1bce3dad
IhorZviahin <igorzvjagin26@gmail.com>
2022-04-27 22:49:42 +0300

    hw4

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Outlet} from "react-router-dom"
+import {Outlet, useSearchParams} from "react-router-dom"
 
 import {postsService} from "../../services";
 import {Post} from "../../components";
@@ -7,16 +7,39 @@ import css from "./PostsPage.module.css"
 
 const PostsPage = () => {
 
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
+
+    let [query, setQuery] = useSearchParams({page: "1"});
+
 
     useEffect(() => {
-        postsService.getAll().then(({data}) => setPosts(data))
-    }, [])
+        postsService.getAll(query.get("page")).then(({data}) => setPosts(data))
+    }, [query])
+
+    let postPage = () => {
+        let page = query.get("page");
+        page = +page -1
+        setQuery({page:page.toString()})
+
+    }
+
+    let nextPage = () => {
+        let page = query.get("page");
+        page = +page +1
+        setQuery({page:page.toString()})
+
+    }
+    let onePage = () => {
+        setQuery({page:2})
+    }
 
     return (
         <div className={css.wrapper}>
             <div>
                 {posts.map(post => <Post key={post.id} post={post}/>)}
+                <button onClick={() => postPage()}>post</button>
+                <button onClick={() => onePage()}>2</button>
+                <button onClick={() => nextPage()}>next</button>
             </div>
             <div className={css.outlet}><Outlet/></div>
 
--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -3,7 +3,12 @@ import {axiosService} from "./axios.service";
 import {urls} from "../constants";
 
 const postsService = {
-    getAll: () => axiosService.get(urls.posts),
+    getAll: (page, _limit = 10) => axiosService.get(urls.posts, {
+        params: {
+            _start:(page-1)*_limit,
+            _limit
+        }
+    }),
     getById: (id) => axiosService.get(`${urls.posts}/${id}`)
 
 }
