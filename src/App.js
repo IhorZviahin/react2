0bea7888cd294387d71ce9e8a71d5a167dbbef13
IhorZviahin <igorzvjagin26@gmail.com>
2022-07-26 21:25:43 +0300

    refresh

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import {Posts, Users} from "./components";
 import {useState} from "react";
 
 function App() {
-    const [postid, setpostid]=useState(null);
-    console.log(postid)
-  return (
-    <div className="App">
+    const [postid, setpostid] = useState(null);
 
-
-      <Users checkid={setpostid}/>
-        {postid &&  <Posts id = {postid}/>}
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Users checkid={setpostid}/>
+            {postid && <Posts id={postid}/>}
+        </div>
+    );
 }
 
 export default App;
