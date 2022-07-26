a8aa38893e41592de537ece0a5def1a7f9aaa365
IhorZviahin <igorzvjagin26@gmail.com>
2022-07-26 23:12:36 +0300

    refresh

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -5,8 +5,7 @@ import {carService} from "../../services";
 const Forms = ({setnewCar}) => {
     const [formError, setFormError] = useState({});
     const {register, handleSubmit, reset} = useForm();
-
-    const submit = async (car) => {
+    const submit = async (car) => { //3 - этап, функция получает информацию Data,  то есть готовый обьект в примере это машинки потому и название car
         try {
             const {data} = await carService.Create(car)
             setnewCar(data)
@@ -17,17 +16,18 @@ const Forms = ({setnewCar}) => {
 
     }
     return (
-        <form onSubmit={handleSubmit(submit)}>
+        <form onSubmit={handleSubmit(submit)}> // handleSubmit - функция сделает preventdefault - что бы страница не обновилась, а так же вкладываем функцию(любое название) submit
             <div>
-                <label> Model <input type="text" {...register("model")}/> </label>
+                <label> Model <input type="text" {...register("model")}/> </label> // register - это обьект и сейчас мы вкладываем в него данные с ключем "model"
             </div>
+
             {formError.model && <span>{formError.model[0]}</span>}
             <div>
                 <label> Price <input type="text" {...register("price")} /> </label>
             </div>
             {formError.price && <span>{formError.model[0]}</span>}
             <div>
-                <label> Year <input type="text" {...register("year")} /> </label>
+                <label> Year <input type="text" {...register("year", {valueAsNumber:true})} /> </label>
             </div>
             {formError.year && <span>{formError.model[0]}</span>}
             <button>save</button>
