d562a17d3cb4756fb6ef30b9356481ee8bc47f84
IhorZviahin <igorzvjagin26@gmail.com>
2022-08-20 17:34:55 +0300

    module

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -2,7 +2,11 @@ import {axiosService} from "./axiosService";
 import {urls} from "../constants";
 
 export const moviesService = {
-    getAll:(page=1)=>axiosService.get(urls.movies,{params: {page}}),
+    getAll:(page=1, sort_by="")=>axiosService.get(urls.movies,{params: {page, sort_by}}),
     getWithGenresId:(id, page=1)=>axiosService.get(`${urls.movies}/?with_genres=${id}`,{params: {page}},),
     getMovieById:(id)=>axiosService.get(`${urls.movie}/${id}`)
 }
+
+
+
+//getVideo:(id)=>axiosService.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(value => value.data),
