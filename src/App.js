744632590503c853138bedb91473655a4a130ee5
IhorZviahin <igorzvjagin26@gmail.com>
2022-08-27 17:37:09 +0300

    module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import "./App.css"
+import {useState} from "react";
+import Modal from "./modal/Modal";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [modalActive, setModalActive] = useState(true)
+
+    return (
+        <div className="App">
+            <main>
+                <button className="open-btn" onClick={()=> {
+                    setModalActive(true)
+                }}>Open module window</button>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
+                    voluptates.</p>
+            </main>
+            <Modal active={modalActive} setActive={setModalActive}/>
+        </div>
+    );
 }
 
 export default App;
