0cd21dce9132734fa0ae19ba4f49aeb52637ff02
IhorZviahin <igorzvjagin26@gmail.com>
2022-08-27 17:40:52 +0300

    modale

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css"
-import {useState} from "react";
-import Modal from "./modal/Modal";
 
 function App() {
 
-    const [modalActive, setModalActive] = useState(true)
-
     return (
-        <div className="App">
-            <main>
-                <button className="open-btn" onClick={()=> {
-                    setModalActive(true)
-                }}>Open modale window</button>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste magni porro, sint sunt
-                    voluptates.</p>
-            </main>
-            <Modal active={modalActive} setActive={setModalActive}/>
+        <div>
+
         </div>
     );
 }
